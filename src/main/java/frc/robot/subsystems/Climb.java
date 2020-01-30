ea48de43648e333b94da1ce137ddd1c2b88582d4
Poly Rabbotics <33707478+petervdonovan@users.noreply.github.com>
2020-01-29 17:35:02 -0800

    finish climb test code

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.RobotMap;
+import frc.robot.controls.MechanismsJoystick;
 
 public class Climb {
     DoubleSolenoid solenoid;
@@ -10,6 +12,13 @@ public class Climb {
         solenoid = RobotMap.climbSolenoid;
     }
     public void run() {
-        
+        if (MechanismsJoystick.getToggleClimbingSystem()) {
+            if(solenoid.get() == Value.kForward) {
+                solenoid.set(Value.kReverse);
+            }
+            else {
+                solenoid.set(Value.kForward);
+            }
+        }
     }
 }
