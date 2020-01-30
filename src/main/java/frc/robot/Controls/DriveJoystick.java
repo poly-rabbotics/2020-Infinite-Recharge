e4406a7566a3d90b52dc16bd47481d4156b76c69
Poly Rabbotics <33707478+petervdonovan@users.noreply.github.com>
2020-01-30 15:53:22 -0800

    change folder names to lower case

--- src/main/java/frc/robot/Controls/DriveJoystick.java
+++ src/main/java/frc/robot/Controls/DriveJoystick.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.Controls;
+package frc.robot.controls;
 
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.RobotMap;
