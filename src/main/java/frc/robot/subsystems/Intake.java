b573d52d21d9f5bc0e074a4f19012d07e19a650f
Jason7042 <jasonmilowhite@gmail.com>
2020-01-30 16:11:59 -0800

    Fixed errors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,7 +7,13 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotMap;
+import frc.robot.Controls.MechanismsJoystick;
 
 /**
  * Add your docs here.
@@ -15,43 +21,44 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Intake extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-private DoubleSolenoid intakePneumatic;
-private PWMVictorSRX intakeWheel;
-boolean out;
-double motorSpeed;
+  private DoubleSolenoid intakePneumatic;
+  private PWMVictorSPX intakeWheel;
+  boolean out;
+  double motorSpeed;
 
-  public Intake() { 
+  public Intake() {
     intakePneumatic = RobotMap.intakeSolenoid;
     intakeWheel = RobotMap.intakeMotor;
     out = false;
     motorSpeed = 0.5;
   }
-  
-  public void runPneumatics(){
-    if(MechanismJoystick.getToggleArm = true && out = false){
+
+  private void runPneumatics() {
+    if (MechanismsJoystick.getToggleArm() == true && out == false) {
       intakePneumatic.set(Value.kForward);
       out = true;
     }
-    else if(MechanismJoystick.getToggleArm = true && out = true){
+    else if(MechanismsJoystick.getToggleArm() == true && out == true) {
       intakePneumatic.set(Value.kReverse);
       out = false;
     }
   }
-  public void runMotor(){
+  private void runMotor(){
     intakeWheel.set(motorSpeed);
-    if(MechanismJoystick.getChangeIntake > 0.1 && motorSpeed < 1){
+    if(MechanismsJoystick.getChangeIntake() > 0.1 && motorSpeed < 1){
       motorSpeed += .005;
     }
-    else if(MechanismJoystick.getChangeIntake < -0.1 && motorSpeed > 0){
+    else if(MechanismsJoystick.getChangeIntake() < -0.1 && motorSpeed > 0){
       motorSpeed -= .005;
     }
-
-    public void run(){
-      runPneumatics();
-      runMotor();
-    }
-
   }
+   public void run(){
+    runMotor();
+    runPneumatics();
+    SmartDashboard.putNumber("Motor Speed: ", motorSpeed);
+   }
+
+  
 
   @Override
   public void initDefaultCommand() {
