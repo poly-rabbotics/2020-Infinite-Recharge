287a4253c15a2b2f9d6bd700551c3f65d94bc05c
Poly Rabbotics <33707478+petervdonovan@users.noreply.github.com>
2020-02-14 20:27:07 -0800

    Fix errors from merge

--- src/main/java/frc/robot/Controls/DriveJoystick.java
+++ src/main/java/frc/robot/Controls/DriveJoystick.java
@@ -40,10 +40,8 @@ public class DriveJoystick {
     return Timer.getFPGATimestamp() - lastMoveTime > 0.75;
     //return joystick.getRawButton(5) || joystick.getRawButton(6);
   }
-}
 
   public static boolean getFront(){
     return joystick.getRawButtonPressed(2);
-
   }
 }
