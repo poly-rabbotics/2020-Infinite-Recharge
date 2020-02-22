9e590b01ad38945aee710363d5e24b6104035a31
Poly Rabbotics <33707478+petervdonovan@users.noreply.github.com>
2020-02-22 12:28:21 -0800

    finish other half of ball count logic

--- src/main/java/frc/robot/subsystems/ConveyorBelt.java
+++ src/main/java/frc/robot/subsystems/ConveyorBelt.java
@@ -9,7 +9,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import frc.robot.controls.MechanismsJoystick;
@@ -17,11 +16,12 @@ import frc.robot.controls.MechanismsJoystick;
 /**
  * Add your docs here.
  */
-public class ConveyorBelt extends Subsystem {
+public class ConveyorBelt {
  PWMVictorSPX lowerConveyor, upperConveyor;
  DigitalInput upperSensor, lowerSensorOne, lowerSensorTwo;
- Double speed, ballCount;
- Boolean lowerBallDetected, shooterIsSpeed;
+ double speed;
+ int ballCount;
+ boolean lowerBallDetected, upperBallDetected, shooterIsSpeed;
 
  public ConveyorBelt(){
   lowerConveyor = RobotMap.lowerConveyorMotor;
@@ -31,25 +31,37 @@ public class ConveyorBelt extends Subsystem {
 
   upperSensor = RobotMap.shooterSensor;
   speed = 0.3;
-  ballCount = 0.0;
+  ballCount = 0;
   lowerBallDetected = false;
+  upperBallDetected = false;
   shooterIsSpeed = false;
-
  }
-
-  public void storeBalls(){
-    if(lowerSensorOne.get() == true || lowerSensorTwo.get() == true && ballCount <= 5){
-      lowerConveyor.set(speed);
-      upperConveyor.set(speed);
+  public void countBalls() {
+    //Check for balls entering the mechanism
+    if(lowerSensorOne.get() == true || lowerSensorTwo.get() == true) {
       lowerBallDetected = true;
-    }
-    else{
-      lowerConveyor.set(0);
-      upperConveyor.set(0);
     }
     if(lowerBallDetected == true && lowerSensorOne.get() == false && lowerSensorTwo.get() == false){
       ballCount += 1;
       lowerBallDetected = false;
+    }
+    //Check for balls leaving the mechanism
+    if(upperSensor.get() == true) {
+      upperBallDetected = true;
+    }
+    if(upperBallDetected == true && upperSensor.get() == false){
+      ballCount -= 1;
+      upperBallDetected = false;
+    }
+  }
+  public void storeBalls(){
+    if(lowerSensorOne.get() == true || lowerSensorTwo.get() == true && getNumberOfBalls() < 5){
+      lowerConveyor.set(speed);
+      upperConveyor.set(speed);
+    }
+    else{
+      lowerConveyor.set(0);
+      upperConveyor.set(0);
     }
   }
 
@@ -63,7 +75,7 @@ public class ConveyorBelt extends Subsystem {
     }
   }
 
-  public void allowShooter(){
+  /*public void allowShooter(){
     if(MechanismsJoystick.getToggleShooter() && shooterIsSpeed){
       lowerConveyor.set(speed);
       upperConveyor.set(speed);
@@ -71,17 +83,13 @@ public class ConveyorBelt extends Subsystem {
     else{
       conveyorOverride();
     }
-  }
+  }*/
 
   public void run(){
     conveyorOverride();
     SmartDashboard.putNumber("Number of Balls in Intake", ballCount);
   }
-
-
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public int getNumberOfBalls() {
+    return ballCount;
   }
 }
