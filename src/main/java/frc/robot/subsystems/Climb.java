dbfe13da1a2ebfee39ca8cbf73a97ebe161e459b
Poly Rabbotics <33707478+petervdonovan@users.noreply.github.com>
2020-02-29 17:17:32 -0800

    Store controller data in variables

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -16,10 +16,22 @@ public class Climb implements Subsystem {
         
     }
     public void run() {
-        if (MechanismsJoystick.getToggleClimbingSystem() && MechanismsJoystick.getAllowClimbingSystem()) {
-            solenoid.set(Value.kForward);
+        boolean toggleClimbingSystem = MechanismsJoystick.getToggleClimbingSystem();
+        boolean allowClimbingSystem  = MechanismsJoystick.getAllowClimbingSystem();
+        if (allowClimbingSystem) {
+            // Mode for endgame
+            if(toggleClimbingSystem) {
+                // Set the solenoid to the state that it is not currently in
+                if(solenoid.get() == Value.kForward) {
+                    solenoid.set(Value.kReverse);
+                }
+                else {
+                    solenoid.set(Value.kForward);
+                }
+            }
         }
-       else{
+        else {
+            // Mode for everything except endgame -- cylinder retracted
             solenoid.set(Value.kReverse);
         }
     }
