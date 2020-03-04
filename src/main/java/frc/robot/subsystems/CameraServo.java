1de966490a7ebed7ee3094983884f772c8fc46e8
Peter Donovan <33707478+petervdonovan@users.noreply.github.com>
2020-03-04 09:14:01 -0800

    Comment CameraServo.java

--- src/main/java/frc/robot/subsystems/CameraServo.java
+++ src/main/java/frc/robot/subsystems/CameraServo.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Servo;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
-
+/**
+ * Class for controlling the servo that rotates the camera.
+*/
 public class CameraServo extends AutoSubsystem {
     Servo cameraServo;
     public CameraServo() {
@@ -17,9 +19,15 @@ public class CameraServo extends AutoSubsystem {
             cameraServo.set(1.0);
         }
     }
+    /**
+     * No special functionality is necessary, so manualRun and advancedRun perform the same function.
+    */
     public void manualRun() {
         run();
     }
+    /**
+     * Fix the camera in one place for the duration of the autonomous period.
+    */
     public void autoRun() {
         cameraServo.set(1.0);
     }
